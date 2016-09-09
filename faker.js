eec4bdbd448a097c99aefb4bfc4e66d0366511e0
root <root@vps183328.ovh.net>
2016-09-09 11:45:22 +0200

    Faker boilerplate

---
+++ faker.js
@@ -0,0 +1,36 @@
+var faker = require('faker');
+var express = require('express')
+var app = express();
+
+app.use(limiter);
+*/
+app.get('/products', function(req, res) {
+
+	var size = req.query.size || 10;
+
+	if (isNaN(parseInt(size))){
+		size = 10;
+	}
+
+	var result = {
+		'products': []
+	};
+
+	for (var i = 0; i <= size; i++) {
+		result.products.push({
+			color: faker.commerce.color(),
+			category: faker.commerce.department(),
+			productName: faker.commerce.productName(),
+			price: faker.commerce.price(),
+			tag: faker.commerce.productAdjective(),
+			productMaterial: faker.commerce.productMaterial(),
+			imageUrl: faker.image.technics(),
+			createdAt : new Date()
+		});
+	}
+
+	res.json(result);
+
+})
+
+app.listen(3000);
